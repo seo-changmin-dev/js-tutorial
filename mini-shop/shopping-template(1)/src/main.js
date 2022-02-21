d160826f49781d03e95390946858892a1a44cdff
scmDev <seo.changmin.dev@gmail.com>
2022-02-21 12:21:35 +0900

    feat: Cloning All

--- mini-shop/shopping-template(1)/src/main.js
+++ mini-shop/shopping-template(1)/src/main.js
@@ -1,26 +1,47 @@
+// Main
+loadItems()
+  .then(items => {
+    console.log(items);
+    displayItems(items);
+    setEventListeners(items);
+  });
+
+// Fetch the items form the JSON file
 function loadItems() {
   return fetch('./data/item-list.json')
   .then(response => response.json())
   .then(json => json.items);
 }
 
+// Update the list with the given items
 function displayItems(items) {
-  const elItems = document.querySelector('.items');
-  elItems.innerHTML = items.map(item => createHTMLString(item)).join('');
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
+// create HTML list item form the given data item
 function createHTMLString(item) {
-  const {type, color, size, sex, image} = item;
+  const {type, gender, size, color, image} = item;
   return `
     <li class="item">
       <img src=${image} alt=${type} class="item__thumbnail">
-      <span class="item__description">${sex}, ${size}</span>
+      <span class="item__description">${gender}, ${size}</span>
     </li>
   `;
 }
 
-// main
-loadItems()
-  .then(items => {
-    displayItems(items);
-  })
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons'); // 이벤트 위임
+
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
+function onButtonClick(event, items) {
+  const {key, value} = event.target.dataset;
+
+  if(key == null || value == null) return;
+
+  displayItems(items.filter(item => item[key] === value));
+}
